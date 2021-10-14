7348aac23e67cd521e70d4c25bbf3fee2a11b44a
iyo-bemoore <imadyoussoufi@gmail.com>
2021-10-14 22:27:13 +0200

    Add User and role mapping

--- src/main/java/be/bemoore/userwithjwt/Utils/Mappings.java
+++ src/main/java/be/bemoore/userwithjwt/Utils/Mappings.java
@@ -3,4 +3,7 @@ package be.bemoore.userwithjwt.Utils;
 public class Mappings {
     public static final String USERS_BASE_URL = "/api";
     public static final String ALL_USERS = "/users";
+    public static final String CREATE_USER = "/user/create" ;
+    public static final String CREATE_ROLE = "/role/create";
+    public static final String ADD_ROLE_TO_USER = "/role/add" ;
 }
--- src/main/java/be/bemoore/userwithjwt/api/UserController.java
+++ src/main/java/be/bemoore/userwithjwt/api/UserController.java
@@ -1,14 +1,16 @@
 package be.bemoore.userwithjwt.api;
 
 import be.bemoore.userwithjwt.Utils.Mappings;
+import be.bemoore.userwithjwt.models.Role;
 import be.bemoore.userwithjwt.models.User;
 import be.bemoore.userwithjwt.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,4 +23,29 @@ public class UserController {
   public ResponseEntity<List<User>>getUsers(){
      return ResponseEntity.ok().body(userService.getUsers());
   }
+    @PostMapping(Mappings.CREATE_USER)
+    public ResponseEntity<User>saveUser(@RequestBody User user){
+
+        return ResponseEntity.created(getCurrentUri("/api/user/create")).body(userService.saveUser(user));
+    }
+    @PostMapping(Mappings.CREATE_ROLE)
+    public ResponseEntity<Role>saveRole(@RequestBody Role role){
+        return ResponseEntity.created(getCurrentUri("/api/role/create")).body(userService.saveRole(role));
+    }
+
+    @PostMapping(Mappings.ADD_ROLE_TO_USER)
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form){
+        userService.addRoleToUser(form.getUsername(),form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+    @org.jetbrains.annotations.NotNull
+    @org.jetbrains.annotations.Contract("_ -> new")
+    private URI getCurrentUri(String path) {
+        return URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path(path).toUriString());
+  }
+}
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
